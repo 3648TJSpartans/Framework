3ea94a76725130b73d8ade0cb4b8f141df1485d8
Pelle-krab <78560773+Pelle-Krab@users.noreply.github.com>
2022-08-03 12:58:10 -0600

    Fixed turret aim

--- src/main/java/frc/robot/Subsystems/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter.java
@@ -30,12 +30,12 @@ public class Shooter implements Subsystem{
 
     public void teleopPeriodic(){
         output.motors.setMotor("FLY_WHEEL", input.getAxis("SHOOT", "OPERATOR") * -0.5);
-        output.motors.setMotor("TURRET_AIM", input.getAxis("TURRET_ANGLE", "OPERATOR") * -0.5);
+        output.motors.setMotor("TURRET_ANGLE", input.getAxis("TURRET_AIM", "OPERATOR") * -0.5);
 
         if(input.getButton("LOAD_BALL", "OPERATOR")){
-            output.servos.setServoAngle("BALL_STOP", 0);
+            output.servos.setServo("BALL_STOP", 0);
         }else{
-            output.servos.setServoAngle("BALL_STOP", 90);
+            output.servos.setServo("BALL_STOP", 1);
         }
 
     }
