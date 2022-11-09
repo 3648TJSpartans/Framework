ee3212e1131d065a538d18c1b67b60038071132e
Cloud22 <mr.cloudy@gmail.com>
2022-11-08 21:00:20 -0700

    motor subsystem

--- src/main/java/frc/robot/framework/subsystems/Motor/Motor.java
+++ src/main/java/frc/robot/framework/subsystems/Motor/Motor.java
@@ -4,10 +4,12 @@ import org.w3c.dom.Element;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.framework.robot.RobotXML;
+import frc.robot.framework.robot.SubsystemCollection;
 import frc.robot.framework.util.ShuffleboardHandler;
 
 public class Motor extends SubsystemBase implements RobotXML{
     ShuffleboardHandler tab;
+    private SubsystemCollection subsystemColection;
 
     public Motor(Element subsystem){
         tab= new ShuffleboardHandler(subsystem.getAttribute("id"));
@@ -17,6 +19,7 @@ public class Motor extends SubsystemBase implements RobotXML{
     @Override
     public void periodic(){
         //System.out.println("Motor periodic");
+        //subsystemColection.motors.setMotor("Turret1", .5);
     }
 
     @Override
@@ -25,7 +28,8 @@ public class Motor extends SubsystemBase implements RobotXML{
 
     @Override
     public void ReadXML(Element element) {
-        System.out.println(element);
+        //XMLUtil.prettyPrint(element);
+        subsystemColection = new SubsystemCollection(element);
     }
 
     @Override
