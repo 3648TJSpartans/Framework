65ca2ac3fb8128e6c6b6612acc81d91ef7ecb4ac
Cloud22 <mr.cloudy@gmail.com>
2022-11-24 15:00:36 -0700

    fix tank drive slightly

--- src/main/java/frc/robot/framework/subsystems/TankDrive/TankDrive_Default.java
+++ src/main/java/frc/robot/framework/subsystems/TankDrive/TankDrive_Default.java
@@ -4,37 +4,44 @@ import org.w3c.dom.Element;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.framework.controller.ControllerBase;
 import frc.robot.framework.robot.*;
 
 public class TankDrive_Default extends CommandBase implements RobotXML{
     
+  private Element myElement;
   private TankDrive tankDrivSubsystem;
   private ControllerBase myController;
+
   private int axisNumberTurn=-1;
   private int axisNumberForward=-1;
   private int axisNumberLeft=-1;
   private int axisNumberRight=-1;
+  
 //<defaultCommand subsystem="TankDrive" axisForward="LEFT_JOYSTICK_Y" axisTurn="RIGHT_JOYSTICK_X" command="TankDrive_Default" scaleX="2" scaleY=".75"></axis>
 
-    public TankDrive_Default(Element element, ControllerBase base){
-      myController=base;
-      tankDrivSubsystem=(TankDrive)RobotInit.GetSubsystem("TankDrive");
-      if (tankDrivSubsystem==null){
-        System.out.println("TankDrive_Default could not find TankDrive Subsystem");
+    public TankDrive_Default(Element element, ControllerBase controller){
+      myElement=element;
+      myController=controller;
+      
+      SubsystemBase temp=RobotInit.GetSubsystem(element.getAttribute("subsystemID"));
+      if (temp==null || !(temp instanceof TankDrive)){
+        System.out.println("TankDrive_Default could not find TankDrive subsystem with id:"+ element.getAttribute("subSystemID"));
         return;
       }
+      tankDrivSubsystem = (TankDrive) temp;
       this.addRequirements(tankDrivSubsystem);
+
       CommandScheduler.getInstance().setDefaultCommand(tankDrivSubsystem, this);
-      if (element.getAttribute("axisForward")!="")
-        axisNumberForward= myController.GetAxisMap().get(element.getAttribute("axisForward"));
-      if (element.getAttribute("axisTurn")!="")
-        axisNumberTurn= myController.GetAxisMap().get(element.getAttribute("axisTurn"));
-      if (element.getAttribute("axisLeft")!="")
-        axisNumberLeft= myController.GetAxisMap().get(element.getAttribute("axisLeft"));
-      if (element.getAttribute("axisRight")!="")
-        axisNumberRight= myController.GetAxisMap().get(element.getAttribute("axisRight"));
-      ReadXML(element);
+      if (myElement.getAttribute("axisForward")!="")
+        axisNumberForward= myController.GetAxisMap().get(myElement.getAttribute("axisForward"));
+      if (myElement.getAttribute("axisTurn")!="")
+        axisNumberTurn= myController.GetAxisMap().get(myElement.getAttribute("axisTurn"));
+      if (myElement.getAttribute("axisLeft")!="")
+        axisNumberLeft= myController.GetAxisMap().get(myElement.getAttribute("axisLeft"));
+      if (myElement.getAttribute("axisRight")!="")
+        axisNumberRight= myController.GetAxisMap().get(myElement.getAttribute("axisRight"));
     }
     
     
@@ -57,7 +64,6 @@ public class TankDrive_Default extends CommandBase implements RobotXML{
 
     @Override
     public void ReadXML(Element element) {
-      
     }
     
     @Override
