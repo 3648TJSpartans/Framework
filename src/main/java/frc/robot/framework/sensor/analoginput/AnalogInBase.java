28181e392c76793c4eb7f2aef606034d3396a91e
Cloud22 <mr.cloudy@gmail.com>
2022-12-03 09:53:40 -0700

    analog inputs

---
+++ src/main/java/frc/robot/framework/sensor/analoginput/AnalogInBase.java
@@ -0,0 +1,9 @@
+package frc.robot.framework.sensor.analoginput;
+
+public interface AnalogInBase {
+    
+    public int getValue();
+    public int getAverageValue();
+    public double getVoltage();
+    public double getAverageVoltage();
+}
---
+++ src/main/java/frc/robot/framework/sensor/analoginput/AnaloginWrapper.java
@@ -0,0 +1,42 @@
+package frc.robot.framework.sensor.analoginput;
+
+import org.w3c.dom.Element;
+
+public class AnaloginWrapper implements AnalogInBase{
+    private AnalogInBase m_analogIn;
+    private Element m_analogInElement;
+
+    public AnaloginWrapper(Element element){
+        m_analogInElement = element;
+        String id = m_analogInElement.getAttribute("id");
+        int port = Integer.parseInt(m_analogInElement.getAttribute("port"));
+        m_analogIn = new Analog_In(port);
+
+        if (m_analogIn == null) {
+            System.out.println("For AnalogInput: " + id + " AnalogInput controller type: " + m_analogInElement.getAttribute("controller") + " was not found!");
+            return;
+        }
+    }
+
+    @Override
+    public int getValue() {
+        return m_analogIn.getValue();
+    }
+
+    @Override
+    public int getAverageValue() {
+        return m_analogIn.getAverageValue();
+    }
+
+    @Override
+    public double getVoltage() {
+        return m_analogIn.getVoltage();
+    }
+
+    @Override
+    public double getAverageVoltage() {
+        return m_analogIn.getAverageVoltage();
+    }
+    
+    
+}
