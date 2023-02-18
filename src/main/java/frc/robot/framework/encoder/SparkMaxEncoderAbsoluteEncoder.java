9101913debd0da71c7df9b7ddff34a5cb95e7c01
Cloud22 <mr.cloudy@gmail.com>
2023-02-18 12:22:52 -0700

    support spark max absolute encoder

---
+++ src/main/java/frc/robot/framework/encoder/SparkMaxEncoderAbsoluteEncoder.java
@@ -0,0 +1,45 @@
+package frc.robot.framework.encoder;
+
+import com.revrobotics.AbsoluteEncoder;
+
+
+public class SparkMaxEncoderAbsoluteEncoder extends EncoderController implements EncoderBase{
+
+    private AbsoluteEncoder encoder;
+
+    public SparkMaxEncoderAbsoluteEncoder(AbsoluteEncoder absoluteEncoder){
+        this.encoder=absoluteEncoder;
+    }
+
+    @Override
+    public int getTicks() {
+        throw new UnsupportedOperationException("SparkMaxEncoderAbsoluteEncoder does not support getTicks()");
+    }
+
+    @Override
+    public double getVelocity() {
+        return encoder.getVelocity();
+    }
+
+    @Override
+    public double getPosition() {
+        return encoder.getPosition();
+    }
+
+    @Override
+    public void setDistancePerPulse(double factor) {
+        encoder.setPositionConversionFactor(factor);
+        encoder.setVelocityConversionFactor(factor / 60);
+    }
+
+    @Override
+    public void resetEncoder() {
+        encoder.setZeroOffset(getPosition());        
+    }
+
+    @Override
+    public void setInverted(boolean inverted) {
+        super.setInverted(inverted);
+        
+    }
+}
---
+++ src/main/java/frc/robot/framework/encoder/SparkMaxEncoderRelativeEncoder.java
@@ -0,0 +1,45 @@
+package frc.robot.framework.encoder;
+
+import com.revrobotics.RelativeEncoder;
+
+
+public class SparkMaxEncoderRelativeEncoder extends EncoderController implements EncoderBase{
+
+    private RelativeEncoder encoder;
+
+    public SparkMaxEncoderRelativeEncoder(RelativeEncoder relativeEncoder){
+        this.encoder=relativeEncoder;
+    }
+
+    @Override
+    public int getTicks() {
+        return encoder.getCountsPerRevolution();
+    }
+
+    @Override
+    public double getVelocity() {
+        return encoder.getVelocity();
+    }
+
+    @Override
+    public double getPosition() {
+        return encoder.getPosition();
+    }
+
+    @Override
+    public void setDistancePerPulse(double factor) {
+        encoder.setPositionConversionFactor(factor);
+        encoder.setVelocityConversionFactor(factor / 60);
+    }
+
+    @Override
+    public void resetEncoder() {
+        encoder.setPosition(0);        
+    }
+
+    @Override
+    public void setInverted(boolean inverted) {
+        super.setInverted(inverted);
+        
+    }
+}
