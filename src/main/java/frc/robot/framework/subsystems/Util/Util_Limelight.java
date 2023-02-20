8b23689563ff28fc4fd19212ebf80995506f4663
PelleKrab <78560773+PelleKrab@users.noreply.github.com>
2023-02-20 15:03:00 -0700

    limelight util test

---
+++ src/main/java/frc/robot/framework/subsystems/Util/Util_Limelight.java
@@ -0,0 +1,41 @@
+package frc.robot.framework.subsystems.Util;
+
+import org.w3c.dom.Element;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.framework.controller.ControllerBase;
+import frc.robot.framework.robot.*;
+import frc.robot.framework.vision.Limelight;
+
+public class Util_Limelight extends CommandBase implements RobotXML {
+
+  private Element myElement;
+  private ControllerBase myController;
+
+  public Util_Limelight(Element element) {
+    myElement = element;
+  }
+
+  @Override
+  public void execute() {
+
+    System.out.println(Limelight.getLimelightX());
+    
+  }
+
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+
+  @Override
+  public void ReadXML(Element element) {
+  }
+
+  @Override
+  public void ReloadConfig() {
+
+  }
+}
